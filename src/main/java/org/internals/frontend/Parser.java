b76dbe774dd1670fa83405481eb2fe593c341597
Jean Claude GOMBANIRO <gombaniro@hotmail.com>
2024-08-02 04:14:37 -0500

    initial commit

---
+++ src/main/java/org/internals/frontend/Parser.java
@@ -0,0 +1,9 @@
+package org.internals.frontend;
+
+/**
+ * <h1>Parser</h1>
+ * <p>A language-independent framework class. This abstract parser class
+ * will be implemented by language-specific subclass.</p>
+ */
+public abstract class Parser {
+}
